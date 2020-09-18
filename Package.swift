30e67742e69c34125a202da70122c733f8b2e657
Josa Vicente Pérez <jvicenpe@gmail.com>
2020-09-18 09:05:09 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,12 +14,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HapticEngine",
-            targets: ["HapticEngine"]),
+            targets: ["HapticEngine"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-            .package(url: "https://github.com/some/package", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
